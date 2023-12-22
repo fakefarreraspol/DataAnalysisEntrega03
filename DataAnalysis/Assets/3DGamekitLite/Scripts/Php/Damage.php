3690f09627d2632a11a7ccf4835c509e3c2f63dd
aNnAm2606 <73582949+aNnAm2606@users.noreply.github.com>
2023-12-22 14:55:25 +0100

    PhpSend

--- DataAnalysis/Assets/3DGamekitLite/Scripts/Php/Damage.php
+++ DataAnalysis/Assets/3DGamekitLite/Scripts/Php/Damage.php
@@ -1,25 +1,65 @@
 <?php
+$servername = "localhost";
+$username   = "juanam15";
+$password   = "9qcnUtYaMqSV";
+$dbname     = "juanam15";
 
-include_once 'settings.php';
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-if(true)
-{    
-     $posX = $_GET['posX']; 
-     $posY = $_GET['posY'];
-     $posZ = $_GET['posZ'];
-	 
+// Check if data is received via POST
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode($_POST['data']);
 
-     $sql = "INSERT INTO Damage (posX,posY,posZ)
-     VALUES ('$posX' ,'$posY','$posZ')";
-     if (mysqli_query($conn, $sql)) {
-        echo "Damage has been added successfully! ";
+    if ($data === null) {
+        // Check if there was an error decoding the JSON data
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                echo "No data received!";
+                break;
+            case JSON_ERROR_DEPTH:
+                echo "Maximum stack depth exceeded!";
+                break;
+            case JSON_ERROR_STATE_MISMATCH:
+                echo "Underflow or the modes mismatch!";
+                break;
+            case JSON_ERROR_CTRL_CHAR:
+                echo "Unexpected control character found!";
+                break;
+            case JSON_ERROR_SYNTAX:
+                echo "Syntax error, malformed JSON!";
+                break;
+            case JSON_ERROR_UTF8:
+                echo "Malformed UTF-8 characters, possibly incorrectly encoded!";
+                break;
+            default:
+                echo "Unknown JSON error!";
+                break;
+        }
+    } else {
+        // Extract data from the JSON object
+        $posX = $data->PosX;
+        $posY = $data->PosY;
+        $posZ = $data->PosZ;
 
-        $last_id = $conn->insert_id;
-        echo "New Damage record created successfully.";
-     } else {
-        echo "Error: " . $sql . ":-" . mysqli_error($conn);
-     }
-     mysqli_close($conn);
+        // Insert data into the database
+        $sql = "INSERT INTO Damage (PosX, PosY, PosZ) VALUES ('$posX', '$posY', '$posZ')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "Damage has been added successfully! ";
+            $last_id = $conn->insert_id;
+            echo "New Damage record created successfully.";
+        } else {
+            echo "Error: " . $sql . ":-" . mysqli_error($conn);
+        }
+    }
+    mysqli_close($conn);
+} else {
+    echo "No data received!";
 }
 ?>
--- DataAnalysis/Assets/3DGamekitLite/Scripts/PhpSend.cs
+++ DataAnalysis/Assets/3DGamekitLite/Scripts/PhpSend.cs
@@ -3,41 +3,27 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 using System;
+using Gamekit3D.Message;
+using static Gamekit3D.Damageable;
 
-public class PhpSend : MonoBehaviour
+public class PhpSend : MonoBehaviour, IMessageReceiver
 {
-    //private Damageable damageable;
+    [SerializeField]
+    private Damageable damageable;
+
+    private string lastId;
+    private string lastSessionId;
+
     private void OnEnable()
     {
-        //damageable = GetComponent<Damageable>();
-
-        //// Subscribe to the damageable's event without modifying the Damageable script
-        //damageable.OnReceiveDamage.AddListener(OnReceiveDamageHandler);
-        // Subscribe to the OnReceiveDamage event using SendMessage
-        gameObject.SendMessage("OnReceiveDamageSubscribe", this, SendMessageOptions.RequireReceiver);
+        damageable.onDamageMessageReceivers.Add(this);
     }
 
-
-    private void OnDisable()
+    IEnumerator PostToServer(string url, string jsonData)
     {
-        // Unsubscribe from the OnReceiveDamage event using SendMessage
-        gameObject.SendMessage("OnReceiveDamageUnsubscribe", this, SendMessageOptions.RequireReceiver);
-    }
-    
-    private void OnPlayerReceiveDamage(string posX, string posY, string posZ)
-    {
-        StartCoroutine(PostToServer("https://citmalumnes.upc.es/~juanam15/Damage.php", posX, posY, posZ));
-    }
-
-    IEnumerator PostToServer(string url, string posX, string posY, string posZ)
-    {
-        // Create the WWWForm and add fields
         WWWForm form = new WWWForm();
-        form.AddField("posX", posX);
-        form.AddField("posY", posY);
-        form.AddField("posZ", posZ);
+        form.AddField("data", jsonData);
 
-        // Send the UnityWebRequest
         using (UnityWebRequest www = UnityWebRequest.Post(url, form))
         {
             yield return www.SendWebRequest();
@@ -54,4 +40,81 @@ public class PhpSend : MonoBehaviour
         }
     }
 
+    IEnumerator PostToServerSession(string url, string jsonData, Action callback)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("data", jsonData);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Check if downloadHandler is not null before accessing text property
+                if (www.downloadHandler != null)
+                {
+                    Debug.Log("Form upload complete! Response: " + www.downloadHandler.text);
+                    // Handle the response as needed
+                }
+                else
+                {
+                    Debug.LogError("Download handler is null!");
+                }
+            }
+        }
+    }
+
+    public void OnReceiveMessage(MessageType type, object sender, object msg)
+    {
+        if (type == MessageType.DAMAGED)
+        {
+            if (msg is Damageable.DamageMessage)
+            {
+                Damageable.DamageMessage damageMessage = (Damageable.DamageMessage)msg;
+
+                Damageable senderDamageable = (Damageable)sender;
+
+                // Check for valid numeric values before sending
+                if (!IsValidNumericValue((int)senderDamageable.transform.position.x) ||
+                    !IsValidNumericValue((int)senderDamageable.transform.position.y) ||
+                    !IsValidNumericValue((int)senderDamageable.transform.position.z))
+                {
+                    Debug.LogError("Invalid position values!");
+                    return;
+                }
+
+                // Create a JSON object with position information
+                PositionData damageData = new PositionData()
+                {
+                    PosX = senderDamageable.transform.position.x,
+                    PosY = senderDamageable.transform.position.y,
+                    PosZ = senderDamageable.transform.position.z
+                };
+
+                string jsonData = JsonUtility.ToJson(damageData);
+
+                // Post JSON data to the server
+                StartCoroutine(PostToServer("https://citmalumnes.upc.es/~juanam15/Damage.php", jsonData));
+            }
+        }
+    }
+
+    // Function to check if a value is a valid numeric value
+    private bool IsValidNumericValue(float value)
+    {
+        // Add additional validation logic if needed
+        return !float.IsNaN(value) && !float.IsInfinity(value);
+    }
+}
+
+public class PositionData
+{
+    public float PosX;
+    public float PosY;  
+    public float PosZ;  
 }
