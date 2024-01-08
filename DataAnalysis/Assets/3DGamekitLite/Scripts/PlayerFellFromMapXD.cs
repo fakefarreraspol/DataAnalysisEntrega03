ca3373626701efb716371782bcf6e3780775880a
Pol <79444349+fakefarreraspol@users.noreply.github.com>
2024-01-08 23:12:41 +0100

    using get component in case the player falls in a acid pool

--- DataAnalysis/Assets/3DGamekitLite/Scripts/PlayerFellFromMapXD.cs
+++ DataAnalysis/Assets/3DGamekitLite/Scripts/PlayerFellFromMapXD.cs
@@ -1,3 +1,4 @@
+using Gamekit3D;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ public class PlayerFellFromMapXD : MonoBehaviour
 {
     private void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.name == "DeathVolume")
+        if(col.gameObject.GetComponent<DeathVolume>())
         {
             FindObjectOfType<SendDeath>().RetrievePlayerData();
         }
